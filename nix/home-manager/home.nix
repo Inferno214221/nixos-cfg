05775304a30b864c5926298d379ce0017b60acd2
inferno214221 <inferno214221@gmail.com>
2024-06-27 19:22:03 +0930

    Seperate VSCodium Config

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -4,6 +4,7 @@
     ./firefox/firefox.nix
     ./gimp/gimp.nix
     ./rofi/rofi.nix
+    ./vscodium/vscodium.nix
     ./xfce/xfce.nix
     ./zsh/zsh.nix
   ];
@@ -100,6 +101,9 @@
       enable = true;
       userName = "Inferno214221";
       userEmail = "inferno214221@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
     };
 
     terminator = {
@@ -127,45 +131,6 @@
           use_theme_colors = true;
         };
       };
-    };
-
-    vscode = {
-      enable = true;
-      package = pkgs.vscodium;
-      # userSettings = {};
-      # keybindings = [];
-      mutableExtensionsDir = false;
-      extensions = (with pkgs.open-vsx; [
-        streetsidesoftware.code-spell-checker-australian-english
-        edwinhuish.better-comments-next
-        bungcip.better-toml
-        uloco.theme-bluloco-dark
-        alefragnani.bookmarks
-        antfu.browse-lite
-        streetsidesoftware.code-spell-checker
-        vadimcn.vscode-lldb
-        moshfeu.diff-merge
-        usernamehw.errorlens
-        dbaeumer.vscode-eslint
-        eamodio.gitlens # To Modify
-        # GML Support
-        # Kali-Dark Theme
-        fwcd.kotlin
-        james-yu.latex-workshop
-        ms-vscode.live-server
-        domdomegg.markdown-inline-preview-vscode # To Modify
-        jnoortheen.nix-ide
-        jeanp413.open-remote-ssh
-        # Pseudocode
-        rust-lang.rust-analyzer
-        gruntfuggly.todo-tree
-        vscode-icons-team.vscode-icons
-        dotjoshjohnson.xml
-      ]) ++ (with pkgs.vscode-marketplace; [
-        redhat.java
-        ms-python.python
-        redhat.vscode-xml
-      ]);
     };
   };
 
---
+++ nix/home-manager/vscodium/vscodium.nix
@@ -0,0 +1,41 @@
+{ inputs, lib, config, pkgs, ... }:
+{
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    # userSettings = {};
+    # keybindings = [];
+    mutableExtensionsDir = false;
+    extensions = (with pkgs.open-vsx; [
+      streetsidesoftware.code-spell-checker-australian-english
+      edwinhuish.better-comments-next
+      bungcip.better-toml
+      uloco.theme-bluloco-dark
+      alefragnani.bookmarks
+      antfu.browse-lite
+      streetsidesoftware.code-spell-checker
+      vadimcn.vscode-lldb
+      moshfeu.diff-merge
+      usernamehw.errorlens
+      dbaeumer.vscode-eslint
+      eamodio.gitlens # To Modify
+      # GML Support
+      # Kali-Dark Theme
+      fwcd.kotlin
+      james-yu.latex-workshop
+      ms-vscode.live-server
+      domdomegg.markdown-inline-preview-vscode # To Modify
+      jnoortheen.nix-ide
+      jeanp413.open-remote-ssh
+      # Pseudocode
+      rust-lang.rust-analyzer
+      gruntfuggly.todo-tree
+      vscode-icons-team.vscode-icons
+      dotjoshjohnson.xml
+    ]) ++ (with pkgs.vscode-marketplace; [
+      redhat.java
+      ms-python.python
+      redhat.vscode-xml
+    ]);
+  };
+}
