b8b6067d37599e5dcb2f2bacd963d6c0c38304c3
inferno214221 <inferno214221@gmail.com>
2025-07-24 13:40:36 +0930

    Updates Kali-Dark theme.

--- nix/home-manager/kali-dark/kali-dark-theme.nix
+++ nix/home-manager/kali-dark/kali-dark-theme.nix
@@ -1,16 +1,35 @@
-{ pkgs }:
+{ pkgs, ... }:
 pkgs.stdenv.mkDerivation {
   name = "kali-dark";
 
   src = pkgs.fetchurl {
     url = "https://gitlab.com/kalilinux/packages/kali-themes/-/archive/kali/2024.2.1/kali-themes-kali-2024.2.1.tar.gz";
-    sha256 = "18a5ea8c812378fffeab4c862fd3453806042cbbef15b66e6f123b827d5e6b63";
+    hash = "sha256-GKXqjIEjeP/+q0yGL9NFOAYELLvvFbZubxI7gn1ea2M=";
   };
 
-  dontUnpack = true;
+  patchPhase = ''
+    # Remove dragon from large window titlebars.
+    cp -f ${./kali-headerbar-logo.png} ./share/themes/Kali-Dark/gtk-3.0/assets/kali-headerbar-logo.png
+    cp -f ${./kali-headerbar-logo-2.png} ./share/themes/Kali-Dark/gtk-3.0/assets/kali-headerbar-logo@2.png
+    cp -f ${./kali-headerbar-logo.png} ./share/themes/Kali-Dark/gtk-3.0/assets/kali-headerbar-logo-dark.png
+    cp -f ${./kali-headerbar-logo-2.png} ./share/themes/Kali-Dark/gtk-3.0/assets/kali-headerbar-logo-dark@2.png
+  '';
+
+  dontBuild = true;
 
   installPhase = ''
-    mkdir -p $out
-    tar -C $out -xzvf $src kali-themes-kali-2024.2.1/share/themes --strip-components=1
+    mkdir -p $out/share
+
+    # Standard theme files.
+    cp -r ./share/themes $out/share/themes
+
+    # Extra theme files including Gedit.
+    cp -r ./share/gtksourceview-3.0 $out/share/gtksourceview-3.0
+    cp -r ./share/gtksourceview-4 $out/share/gtksourceview-4
+    cp -r ./share/gtksourceview-5 $out/share/gtksourceview-5
+
+    # Not used atm, but should be helpful for qt programs.
+    cp -r ./share/qt5ct $out/share/qt5ct
+    cp -r ./share/qt6ct $out/share/qt6ct
   '';
 }
