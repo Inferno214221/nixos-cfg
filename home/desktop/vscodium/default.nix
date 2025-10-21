c40d3c7f5e5607fba7946882a1f03a82a9c50678
inferno214221 <inferno214221@gmail.com>
2025-10-21 21:42:18 +1030

    Updates VSCodium config for update.

--- home/desktop/vscodium/default.nix
+++ home/desktop/vscodium/default.nix
@@ -1,13 +1,13 @@
 { inputs, lib, config, pkgs, ... }:
-{
+with pkgs.unstable; {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = vscodium;
     # userSettings = {};
     # keybindings = [];
     mutableExtensionsDir = false;
     # TODO: Should totally define profiles
-    profiles.default.extensions = (with pkgs.open-vsx; [
+    profiles.default.extensions = (with open-vsx; [
       streetsidesoftware.code-spell-checker-australian-english
       edwinhuish.better-comments-next
       tamasfe.even-better-toml
@@ -15,36 +15,37 @@
       alefragnani.bookmarks
       antfu.browse-lite
       streetsidesoftware.code-spell-checker
-      # vadimcn.vscode-lldb
       moshfeu.diff-merge
-      usernamehw.errorlens
       dbaeumer.vscode-eslint
       eamodio.gitlens # To Modify
       fwcd.kotlin
       ms-vscode.live-server
       jnoortheen.nix-ide
       jeanp413.open-remote-ssh
-      # rust-lang.rust-analyzer
       gruntfuggly.todo-tree
-      vscode-icons-team.vscode-icons
+      vscode-icons-team.vscode-icons # Temporarily Broken
       dotjoshjohnson.xml
       mkhl.direnv
       myriad-dreamin.tinymist
-    ]) ++ (with pkgs.vscode-marketplace; [
+    ]) ++ (with vscode-marketplace; [
       # redhat.java
       ms-python.python
+      sandcastle.vscode-open
       # redhat.vscode-xml
-    ] ++ (with pkgs; [
+    # ]) ++ (with vscode-marketplace-release; [
+    #   github.copilot
+    #   github.copilot-chat
+    ] ++ (with pkgs; [ # These use stable pkgs
       mine.kali-dark-vscode
       # fenix version of rust-analyzer
       vscode-extensions.rust-lang.rust-analyzer-nightly
-      (pkgs.vscode-utils.buildVscodeExtension {
+      (vscode-utils.buildVscodeExtension {
         name = "markdown-editor";
         pname = "markdown-editor";
         version = "0.1.13";
         src = ./markdown-editor.zip;
         # TODO: download (and build)?
-        # src = pkgs.fetchFromGitHub {
+        # src = fetchFromGitHub {
         #   owner = "inferno214221";
         #   repo = "vscode-markdown-editor";
         #   rev = "20e7109dd5ae07e615a3747bc886572342616bb3";
@@ -54,6 +55,13 @@
         vscodeExtName = "markdown-editor";
         vscodeExtUniqueId = "zaaack.markdown-editor";
       })
+    # ]) ++ (vscode-utils.extensionsFromVscodeMarketplace [
+    #   {
+    #     name = "copilot-chat";
+    #     publisher = "GitHub";
+    #     version = "0.23.2024120501";
+    #     hash = "sha256-X67qoGmG4uZI0fQYDFSupNzLKO8apXtZTmWfJbIKQUE=";
+    #   }
     ]));
   };
 }
